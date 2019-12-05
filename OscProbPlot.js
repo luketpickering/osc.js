be9d90599135c02fe080d2fcbc4337bf30c5a8b2
Luke Pickering <luketpickering@googlemail.com>
2019-12-05 09:42:06 -0600

    Adds ND280 simple app

--- OscProbPlot.js
+++ OscProbPlot.js
@@ -5,49 +5,49 @@ class OscProbPlot {
     this.hvr = undefined;
   }
 
-  DrawAxes(el, xmin_GeV = 0, xmax_GeV = 10, ymin = 0, ymax = 1, ylabel="\\(P_{\\textrm{osc.}}\\)") {
+  DrawAxes(el, xmin_GeV = 0, xmax_GeV = 10, ymin = 0, ymax = 1, ylabel = "\\(P_{\\textrm{osc.}}\\)") {
     this.width = 500;
     this.height = 400;
-    this.margin = {top : 20, right : 20, bottom : 75, left : 95};
+    this.margin = { top: 20, right: 20, bottom: 75, left: 95 };
     this.tot_width = this.width + this.margin.left + this.margin.right;
     this.tot_height = this.height + this.margin.top + this.margin.bottom;
 
     let xScale = d3.scaleLinear()
-                     .domain([ xmin_GeV, xmax_GeV ]) // input
-                     .range([ 0, this.width ]);      // output
+      .domain([xmin_GeV, xmax_GeV]) // input
+      .range([0, this.width]);      // output
     this.xScale = xScale;
 
     let yScale = d3.scaleLinear()
-                     .domain([ ymin, ymax ])     // input
-                     .range([ this.height, 0 ]); // output
+      .domain([ymin, ymax])     // input
+      .range([this.height, 0]); // output
     this.yScale = yScale;
 
     this.lineGen = d3.line()
-                       .x(function(d) { return xScale(d[0]); })
-                       .y(function(d) { return yScale(d[1]); });
+      .x(function (d) { return xScale(d[0]); })
+      .y(function (d) { return yScale(d[1]); });
 
     this.svg = d3.select(el)
-                   .append("svg")
-                   .attr("width", this.tot_width)
-                   .attr("height", this.tot_height)
-                   .append("g")
-                   .attr("transform", "translate(" + this.margin.left + "," +
-                                          this.margin.top + ")");
+      .append("svg")
+      .attr("width", this.tot_width)
+      .attr("height", this.tot_height)
+      .append("g")
+      .attr("transform", "translate(" + this.margin.left + "," +
+        this.margin.top + ")");
 
     this.svg.append("g")
-        .attr("class", "x_axis biglabel")
-        .attr("transform", "translate(0," + this.height + ")")
-        .call(d3.axisBottom(xScale).tickArguments([ 5 ]));
+      .attr("class", "x_axis biglabel")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(d3.axisBottom(xScale).tickArguments([5]));
 
     RenderLatexLabel(this.svg.append("text").text("\\(E_{\\nu} \\textrm{(GeV)}\\)"),
-                this.svg, "25ex", "10ex", this.width*0.4,this.height*0.72, 1.5, 1.5);
+      this.svg, "25ex", "10ex", this.width * 0.4, this.height * 0.72, 1.5, 1.5);
 
     this.svg.append("g")
-        .attr("class", "y_axis biglabel")
-        .call(d3.axisLeft(yScale).tickArguments([ 3 ]));
+      .attr("class", "y_axis biglabel")
+      .call(d3.axisLeft(yScale).tickArguments([3]));
 
     RenderLatexLabel(this.svg.append("text").text(ylabel),
-                this.svg, "25ex", "10ex", -100,-65, 1.5, 1.5, -90);
+      this.svg, "25ex", "10ex", -100, -65, 1.5, 1.5, -90);
   }
 
   ScrubCurve(curve) {
@@ -61,44 +61,54 @@ class OscProbPlot {
 
   AddCurve(curve, tool_tip_html) {
     this.ScrubCurve(curve);
-    let tooltip = d3.select("body")
-                      .append("div")
-                      .attr("class", "tooltip")
-                      .style("opacity", 0);
-    tooltip.html(tool_tip_html);
-    MathJax.Hub.Queue([ "Typeset", MathJax.Hub, tooltip.node() ]);
+    let tooltip = undefined;
+    if (tool_tip_html) {
+      tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+      tooltip.html(tool_tip_html);
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub, tooltip.node()]);
+    }
 
     this.Curves.push({
-      path : this.svg.append("path")
-                 .attr("d",
-                       this.lineGen(curve.data)) // 11. Calls the line generator
-                 .attr("class", `osc_line ${curve.line_class}`)
-                 .on("mouseover",
-                     function() {
-                       console.log("over");
-                       tooltip.transition().duration(200).style("opacity", .9);
-                       tooltip.style("left", (d3.event.pageX) + "px")
-                           .style("top", (d3.event.pageY - 28) + "px");
-                     })
-                 .on("mouseout",
-                     function() {
-                       tooltip.transition().duration(500).style("opacity", 0);
-                     }),
-      tooltip : tooltip
+      path: this.svg.append("path")
+        .attr("d",
+          this.lineGen(curve.data)) // 11. Calls the line generator
+        .attr("class", `osc_line ${curve.line_class}`)
+        .on("mouseover",
+          function () {
+            if (tooltip) {
+              tooltip.transition().duration(200).style("opacity", .9);
+              tooltip.style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            }
+          })
+        .on("mouseout",
+          function () {
+            if (tooltip) {
+              tooltip.transition().duration(500).style("opacity", 0);
+            }
+          }),
+      tooltip: tooltip
     });
   };
 
   RemoveNext() {
     if (this.next != undefined) {
       this.next.path.remove();
-      this.next.tooltip.remove();
+      if (this.next.tooltip) {
+        this.next.tooltip.remove();
+      }
       this.next = undefined;
     }
   }
   ClearAll() {
     for (let i = 0; i < this.Curves.length; ++i) {
       this.Curves[i].path.remove();
-      this.Curves[i].tooltip.remove();
+      if (this.Curves[i].tooltip) {
+        this.Curves[i].tooltip.remove();
+      }
     }
     this.Curves = [];
     this.RemoveNext();
@@ -107,29 +117,37 @@ class OscProbPlot {
   ShowNext(curve, tool_tip_html) {
     this.ScrubCurve(curve);
     this.RemoveNext();
-    let tooltip = d3.select("body")
-                      .append("div")
-                      .attr("class", "tooltip")
-                      .style("opacity", 0);
-    tooltip.html(tool_tip_html);
-    MathJax.Hub.Queue([ "Typeset", MathJax.Hub, tooltip.node() ]);
+
+    let tooltip = undefined;
+    if (tool_tip_html) {
+      tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+      tooltip.html(tool_tip_html);
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub, tooltip.node()]);
+    }
 
     this.next = {
-      path : this.svg.append("path")
-                 .attr("d",
-                       this.lineGen(curve.data)) // 11. Calls the line generator
-                 .attr("class", `osc_line ${curve.line_class}`)
-                 .on("mouseover",
-                     function() {
-                       tooltip.transition().duration(200).style("opacity", .9);
-                       tooltip.style("left", (d3.event.pageX) + "px")
-                           .style("top", (d3.event.pageY - 28) + "px");
-                     })
-                 .on("mouseout",
-                     function() {
-                       tooltip.transition().duration(500).style("opacity", 0);
-                     }),
-      tooltip : tooltip
+      path: this.svg.append("path")
+        .attr("d",
+          this.lineGen(curve.data)) // 11. Calls the line generator
+        .attr("class", `osc_line ${curve.line_class}`)
+        .on("mouseover",
+          function () {
+            if (tooltip) {
+              tooltip.transition().duration(200).style("opacity", .9);
+              tooltip.style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            }
+          })
+        .on("mouseout",
+          function () {
+            if (tooltip) {
+              tooltip.transition().duration(500).style("opacity", 0);
+            }
+          }),
+      tooltip: tooltip
     };
   }
   AddHover(curve) {
@@ -138,10 +156,10 @@ class OscProbPlot {
       this.hvr.remove();
     }
     this.hvr =
-        this.svg.append("path")
-            .attr("d",
-                  this.lineGen(curve.data)) // 11. Calls the line generator
-            .attr("class", `osc_line ${curve.line_class} dashed_line`);
+      this.svg.append("path")
+        .attr("d",
+          this.lineGen(curve.data)) // 11. Calls the line generator
+        .attr("class", `osc_line ${curve.line_class} dashed_line`);
   }
   RemoveHover() {
     if (this.hvr != undefined) {
---
+++ ndapp.js
@@ -0,0 +1,131 @@
+"use strict";
+
+class ndapp {
+
+  constructor() {
+
+    this.Widgets = {};
+    this.Widgets.NDCombPlot_El = document.getElementById("NDCombPlot");
+    this.Widgets.ConstraintControls =
+      document.getElementById("ConstraintControls");
+
+    this.ebinning = [0, 0.111111, 0.222222, 0.333333, 0.444444, 0.555556, 0.666667, 0.777778, 0.888889, 1, 1.11111, 1.22222, 1.33333, 1.44444, 1.55556, 1.66667, 1.77778, 1.88889, 2, 2.11111, 2.22222, 2.33333, 2.44444, 2.55556, 2.66667, 2.77778, 2.88889, 3, 3.11111, 3.22222, 3.33333, 3.44444, 3.55556, 3.66667, 3.77778, 3.88889, 4, 4.11111, 4.22222, 4.33333, 4.44444, 4.55556, 4.66667, 4.77778, 4.88889, 5];
+
+    this.ND280Flux = [62331, 250248, 615333, 1.18128e+06, 1.81861e+06, 1.86869e+06, 1.42664e+06, 827517, 422623, 241665, 163120, 121161, 97147, 78615, 64391, 55346, 49211, 43076, 37606, 35057, 29522, 27751, 25408, 23698, 22977, 20517, 19658, 19880, 17788, 17849, 16626, 16418, 15611, 14884, 14699, 13962, 13388, 12746, 12416, 11738, 10945, 10710, 9968, 9238, 9022];
+    this.WAGASCIFlux = [79342, 226505, 384307, 635388, 982814, 1.29075e+06, 1.29934e+06, 1.19332e+06, 907440, 619272, 365030, 281456, 235288, 160517, 160107, 123176, 134455, 99257, 82038, 53965, 90766, 43453, 39598, 49433, 43757, 35984, 29048, 17177, 34714, 16394, 15459, 29098, 26839, 36603, 14646, 16347, 16592, 19667, 10812, 20764, 23394, 14351, 21162, 5592, 14593];
+    this.INGRIDFlux = [23476, 88781, 191314, 304484, 409112, 503917, 572827, 635323, 650695, 635324, 623415, 609273, 587037, 543533, 495862, 442211, 385173, 329805, 277218, 239630, 205970, 173946, 148096, 126812, 107723, 90209, 77254, 64427, 52612, 43302, 36552, 30249, 25090, 20820, 17469, 15151, 11920, 10858, 9714, 9009, 7865, 7246, 7262, 6766, 6249];
+
+    this.coeffs = { "ND280": 1, "WAGASCI": 1, "INGRID": 1 };
+
+    this.color_class_wheel = [
+      "ColorWheel1", "ColorWheel2", "ColorWheel3", "ColorWheel4", "ColorWheel5"
+    ];
+    this.wheel_idx = 0;
+  }
+
+  AddCurve(plot, params, curve_state = 0) {
+
+    let combflux = {
+      meta: {
+        id: 0,
+        name: "combflux"
+      },
+      data: [],
+      line_class: this.color_class_wheel[this.wheel_idx]
+    };
+
+    let ND280F = {
+      meta: {
+        id: 0,
+        name: "ND280F"
+      },
+      data: [],
+      line_class: this.color_class_wheel[this.wheel_idx + 1]
+    };
+    let WAGASCIF = {
+      meta: {
+        id: 0,
+        name: "WAGASCIF"
+      },
+      data: [],
+      line_class: this.color_class_wheel[this.wheel_idx + 2]
+    };
+    let INGRIDF = {
+      meta: {
+        id: 0,
+        name: "INGRIDF"
+      },
+      data: [],
+      line_class: this.color_class_wheel[this.wheel_idx + 3]
+    };
+
+    let ND280W = params["ND280"];
+    let WAGASCIW = params["WAGASCI"];
+    let INGRIDW = params["INGRID"];
+
+    for (let i = 0; i < this.ebinning.length - 1; ++i) {
+      let p = [];
+      p[0] = (this.ebinning[i] + this.ebinning[i + 1]) / 2.0;
+      p[1] = (this.ND280Flux[i] * ND280W + this.WAGASCIFlux[i] * WAGASCIW + this.INGRIDFlux[i] * INGRIDW) * 1E-6;
+      if (!isNaN(p[1])) {
+        combflux.data.push(p);
+        ND280F.data.push([p[0], this.ND280Flux[i] * ND280W * 1E-6]);
+        WAGASCIF.data.push([p[0], this.WAGASCIFlux[i] * WAGASCIW * 1E-6]);
+        INGRIDF.data.push([p[0], this.INGRIDFlux[i] * INGRIDW * 1E-6]);
+      }
+    }
+
+    if (curve_state === 2) {
+      plot.AddHover(combflux);
+    } else if (curve_state === 1) {
+      plot.ClearAll();
+      plot.ShowNext(combflux);
+      plot.AddCurve(ND280F);
+      plot.AddCurve(WAGASCIF);
+      plot.AddCurve(INGRIDF);
+    } else {
+      plot.AddCurve(combflux);
+    }
+  }
+
+  Init() {
+
+    this.InitializeConstraintControls();
+
+    this.Widgets.NDCombPlot = new OscProbPlot();
+    this.Widgets.NDCombPlot.DrawAxes(this.Widgets.NDCombPlot_El, 0, 5, -10, 20, "\\(\\textrm{Combined flux}\\)");
+
+    AddNewConstrainWidgetPoints(this.coeffs);
+    this.AddCurve(this.Widgets.NDCombPlot, this.coeffs, 1);
+
+  }
+
+  InitializeConstraintControls() { // On click
+    InitializeNDConstraintWidgets(
+      this.Widgets.ConstraintControls,
+      (chg_par) => {
+        this.coeffs[chg_par.x_name] = chg_par.x_value
+        this.coeffs[chg_par.y_name] = chg_par.y_value
+
+        let coeffcopy = Object.create(this.coeffs);
+
+        coeffcopy.point_class = this.color_class_wheel[this.wheel_idx];
+
+        this.AddCurve(this.Widgets.NDCombPlot, coeffcopy, 1);
+
+        return coeffcopy;
+      },
+      (chg_par) => { // On drag
+
+        let coeffcopy = Object.create(this.coeffs);
+        coeffcopy[chg_par.x_name] = chg_par.x_value
+        coeffcopy[chg_par.y_name] = chg_par.y_value
+
+        this.AddCurve(this.Widgets.NDCombPlot, coeffcopy, 2);
+      },
+      () => { // Off drag
+        this.Widgets.NDCombPlot.RemoveHover();
+      });
+  }
+
+};
